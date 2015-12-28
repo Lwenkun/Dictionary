4fe38a633e296584687509d60609f2ab4090ae51
Lwenkun <Lwenkun666@gmail.com>
2015-12-28 13:32:28 +0800

    initial commit

---
+++ app/src/main/java/com/lwenkun/dictionary/CollectionActivity.java
@@ -0,0 +1,17 @@
+package com.lwenkun.dictionary;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 15119 on 2015/12/27.
+ */
+public class CollectionActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedOnInstanceState) {
+        super.onCreate(savedOnInstanceState);
+
+    }
+
+}
